40827619c18343258de17554c587e05bb2c9a5ca
Samuel Brehm <samuelbrehm@gmail.com>
2019-09-17 13:53:27 -0300

    Initial config

---
+++ controller/newsController.ts
@@ -0,0 +1,59 @@
+import NewsService from "../services/newsService";
+import * as HttpStatus from "http-status";
+
+class NewsController {
+  sendResponse = function(res, statusCode, data) {
+    res.status(statusCode).json({ result: data });
+  };
+
+  get(req, res) {
+    NewsService.get()
+      .then(news => this.sendResponse(res, HttpStatus.OK, news))
+      .catch(error => console.error.bind(console, `Error ${error}`));
+  }
+
+  getById(req, res) {
+    const _id = req.params.id;
+
+    NewsService.getById(_id)
+      .then(news => this.sendResponse(res, HttpStatus.OK, news))
+      .catch(error => console.error.bind(console, `Error ${error}`));
+  }
+
+  create(req, res) {
+    let vm = req.body;
+
+    NewsService.create(vm)
+      .then(news =>
+        this.sendResponse(res, HttpStatus.OK, "Notícia cadastrada com sucesso!")
+      )
+      .catch(error => console.error.bind(console, `Error ${error}`));
+  }
+
+  update(req, res) {
+    const _id = req.params.id;
+    let news = req.body;
+
+    NewsService.update(_id, news)
+      .then(news =>
+        this.sendResponse(
+          res,
+          HttpStatus.OK,
+          `${news.title} foi atualizada com sucesso!`
+        )
+      )
+      .catch(error => console.error.bind(console, `Error ${error}`));
+  }
+
+  delete(req, res) {
+    const _id = req.params.id;
+
+    NewsService.delete(_id)
+      .then(() =>
+        this.sendResponse(res, HttpStatus.OK, `Notícia atualizada com sucesso!`)
+      )
+      .catch(error => console.error.bind(console, `Error ${error}`));
+  }
+}
+
+export default new NewsController();
