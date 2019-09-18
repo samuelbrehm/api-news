aca867307c83a09812bb73ea2fc412ebb9d3d24f
Samuel Brehm <samuelbrehm@gmail.com>
2019-09-18 18:04:30 -0300

    Cors

--- startUp.ts
+++ startUp.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as cors from "cors";
 
 import Database from "./infra/db";
 import NewsController from "./controller/newsController";
@@ -17,7 +18,17 @@ class StartUp {
     this.routes();
   }
 
+  enableCors() {
+    const options: cors.CorsOptions = {
+      methods: "GET, OPTIONS, PUT, POST, DELETE",
+      origin: "*"
+    };
+
+    this.app.use(cors(options));
+  }
+
   middler() {
+    this.enableCors();
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
   }
